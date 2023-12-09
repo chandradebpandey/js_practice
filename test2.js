b9e57dd85fce761146f679accfce6ca26c064659
Chandradeb Pandey <96076466+chandradebpandey@users.noreply.github.com>
2023-12-09 11:43:37 +0000

    UptodataTypes

---
+++ test2.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer verion
+
+// alert("hello") alert(3+3) we are using nodejs, not browser
+
+// console.log(3 + 3)
+// console.log("Hello")
+
+let name = "Chandra"
+let age = 18
+let isLoggedIn = false;
+let state = null
+let temp;
+
+console.log(typeof "Hello")    // string
+console.log(typeof undefined) // undefined
+console.log(typeof null)     // object
+//console.table([name,age,isLoggedIn,state,temp]);
