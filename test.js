83dc444c7b86a9595f2035585fd4fb220b1ecc3d
Chandradeb Pandey <96076466+chandradebpandey@users.noreply.github.com>
2023-12-09 06:44:18 +0000

    Container config done

--- test.js
+++ test.js
@@ -4,15 +4,17 @@ let accountEmail = "chandradebpandey@gmail.com"
 var accountPassword = "123456"
 accountCity = "Jaipur"
 let accountState;
+accountState = "Hyderabad";
 
 //accountId = 2
 accountEmail = "xyz@gmail.com"
 accountPassword = "4321"
 
 
-// console.log(accountId)
-// console.log(accountEmail)
-// console.log(accountPassword)
-// console.log(accountCity)
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
 
+//To print the everything in tabular form]
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
