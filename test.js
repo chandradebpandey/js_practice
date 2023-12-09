73f29915c04a09919fcc1b6617490311352fd6d9
Chandradeb Pandey <96076466+chandradebpandey@users.noreply.github.com>
2023-12-09 06:41:19 +0000

    container config done

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-console.log("Chandradeb")
+console.log("Welcome to Js")
+const accountId = 144553
+let accountEmail = "chandradebpandey@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+accountEmail = "xyz@gmail.com"
+accountPassword = "4321"
+
+
+// console.log(accountId)
+// console.log(accountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
