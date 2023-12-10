fd7d16191b7d18d9a343935dab0583da30a13a1d
Chandradeb Pandey <96076466+chandradebpandey@users.noreply.github.com>
2023-12-10 05:38:06 +0000

    Conversion of data Types

---
+++ test3.js
@@ -0,0 +1,21 @@
+let score = true
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber)
+
+let isLoggedIn = ""
+ 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof(stringNumber))
