af13e60d68fe9d4bf599cd49c48dd887603f14b6
Chandradeb Pandey <96076466+chandradebpandey@users.noreply.github.com>
2023-12-12 10:02:46 +0000

    Upto Memory Done

--- BegineerLevel/test3.js
+++ BegineerLevel/test3.js
@@ -1,21 +1,42 @@
-let score = true
+// let score = true
 
-// console.log(typeof(score));
+// // console.log(typeof(score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof(valueInNumber));
+// // let valueInNumber = Number(score)
+// // console.log(typeof(valueInNumber));
 
-// console.log(valueInNumber)
+// // console.log(valueInNumber)
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
  
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(isLoggedIn);
-// console.log(booleanIsLoggedIn)
+// // let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(isLoggedIn);
+// // console.log(booleanIsLoggedIn)
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+// ********** Memory Example ********************
+let myYoutube = "hiteshchaudhuridotcom"
+let anotherName = myYoutube;
+anotherName = "chaiaurCode";
+
+console.log(myYoutube)
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "xyz@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
